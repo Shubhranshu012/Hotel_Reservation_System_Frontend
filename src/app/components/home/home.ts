da74ba0d3df7602162b0beeda432a39963c38a3f
Shubhranshu012 <shubhranshu.satpathy@gmail.com>
2025-12-31 08:41:52 +0530

    Working on Home Page

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit{
+  constructor(private cdr: ChangeDetectorRef) {}
+  images = [
+    'img1.jpg',
+    'img2.jpg',
+    'img3.jpg',
+    'img4.jpg',
+    'img5.jpg'
+  ];
+  currentIndex = 0;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      this.cdr.detectChanges();
+    }, 15000); 
+  }
 
 }
