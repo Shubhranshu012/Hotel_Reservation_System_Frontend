56f53eb729bc64eaa2dd58a793601dd443695bc5
Shubhranshu012 <shubhranshu.satpathy@gmail.com>
2025-12-31 23:06:56 +0530

    Working on Sign Up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { Signin } from './components/signin/signin';
 
 export const routes: Routes = [
-    {path:"",component:Home},{path:"home",component:Home}
+    {path:"",component:Home},{path:"home",component:Home},
+    {path:"signin",component:Signin}
 ];
